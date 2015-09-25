b6dad2f7652d08cb28cc8740b1df80ba5dcee708
Salvador <salvadoredgar99@gmail.com>
2015-09-24 19:43:54 -0500

    Add fragments to the NavigationDrawer

---
+++ app/src/main/java/com/icaboalo/toolbarexampledevfbatch6/Fragment2.java
@@ -0,0 +1,21 @@
+package com.icaboalo.toolbarexampledevfbatch6;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Edgar Salvador Maurilio on 24/09/2015.
+ */
+public class Fragment2 extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_2, container, false);
+    }
+}
--- app/src/main/java/com/icaboalo/toolbarexampledevfbatch6/HomeActivity.java
+++ app/src/main/java/com/icaboalo/toolbarexampledevfbatch6/HomeActivity.java
@@ -2,6 +2,9 @@ package com.icaboalo.toolbarexampledevfbatch6;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -20,32 +23,9 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         setupToolbar();
+        setupDrawer();
 
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-                        switch (menuItem.getItemId()) {
-                            case R.id.action_explore:
-                                Toast.makeText(HomeActivity.this, R.string.action_explore, Toast.LENGTH_SHORT).show();
-                                break;
-                            case R.id.action_people:
-                                Toast.makeText(HomeActivity.this, R.string.action_people, Toast.LENGTH_SHORT).show();
-                                break;
-                            case R.id.action_map:
-                                Toast.makeText(HomeActivity.this, R.string.action_map, Toast.LENGTH_SHORT).show();
-                                break;
-                        }
-
-                        drawerLayout.closeDrawers();
-
-                        return false;
-                    }
-                });
-
+        replaceFragment(new Fragment1());
 
     }
 
@@ -54,6 +34,47 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+    }
+
+
+    private void setupDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+
+                        Fragment fragment = null;
+                        switch (menuItem.getItemId()) {
+                            case R.id.action_explore:
+                                fragment = new Fragment1();
+                                Toast.makeText(HomeActivity.this, R.string.action_explore, Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.action_people:
+                                fragment = new Fragment2();
+                                Toast.makeText(HomeActivity.this, R.string.action_people, Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.action_map:
+                                fragment = new Fragment3();
+                                Toast.makeText(HomeActivity.this, R.string.action_map, Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+
+                        replaceFragment(fragment);
+                        drawerLayout.closeDrawers();
+
+                        return false;
+                    }
+                });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.contaiter, fragment);
+        fragmentTransaction.commit();
     }
 
 
